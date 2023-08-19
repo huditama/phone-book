c461243f18c80b81e615f7989689f607a832c49d
Reyhan Huditama <reyhan21098@gmail.com>
2023-08-19 20:41:46 +0700

    refactor: clean up directory

---
+++ src/app/App.styles.tsx
@@ -0,0 +1,15 @@
+import { css } from '@emotion/css';
+
+const Styles = {
+  mainLayout: css({
+    boxShadow: '0 0 48px 0 rgba(0, 0, 0, .2)',
+    backgroundColor: '#fff',
+    minHeight: '100vh',
+    maxWidth: '500px',
+    margin: '0 auto',
+    position: 'relative',
+    overflow: 'hidden'
+  })
+};
+
+export { Styles };
---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Styles } from './App.styles';
+
+function App() {
+  return (
+    <div className={Styles.mainLayout}>
+      <span>Hello World</span>
+    </div>
+  );
+}
+
+export default App;
