70bd02d6fb2608f243071307be7551fe6e58d611
Reyhan Huditama <reyhan21098@gmail.com>
2023-08-23 17:27:40 +0700

    feat: integrate with add contact API

--- src/screens/Form/Form.tsx
+++ src/screens/Form/Form.tsx
@@ -1,29 +1,35 @@
+/* eslint-disable no-alert */
 /* eslint-disable react/no-array-index-key */
 import React, {
   ChangeEvent, FC, FormEvent, useEffect, useState,
 } from 'react';
+import { useMutation } from '@apollo/client';
+import { useNavigate } from 'react-router-dom';
 
 import Header from '../../components/Header/Header';
 import FormInput from '../../components/FormInput/FormInput';
 import { Styles } from './Form.styles';
 import { HeaderType } from '../../types/types';
+import { ADD_CONTACT_WITH_PHONES } from '../../utils/queries';
 
 interface Contact {
-  firstName: string;
-  lastName: string;
-  phoneNumbers: string[];
+  first_name: string;
+  last_name: string;
+  phones: string[];
 }
 
 const Form: FC = () => {
   const [contact, setContact] = useState<Contact>({
-    firstName: '',
-    lastName: '',
-    phoneNumbers: [''],
+    first_name: '',
+    last_name: '',
+    phones: [''],
   });
   const [isValid, setIsValid] = useState(false);
+  const [addContact, { loading, error, data }] = useMutation(ADD_CONTACT_WITH_PHONES);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const isFormFilled = contact.firstName && contact.lastName && contact.phoneNumbers.some((number) => number !== '');
+    const isFormFilled = contact.first_name && contact.last_name && contact.phones.some((number) => number !== '');
 
     if (isFormFilled) {
       setIsValid(true);
@@ -31,6 +37,16 @@ const Form: FC = () => {
       setIsValid(false);
     }
   }, [contact]);
+
+  useEffect(() => {
+    if (error) {
+      alert('Oops, something went wrong. Please try again!');
+    }
+
+    if (!loading && !error && data) {
+      navigate(`/contact/${data.insert_contact.returning[0].id}`, { replace: true });
+    }
+  }, [loading, error, data]);
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
@@ -43,19 +59,19 @@ const Form: FC = () => {
   const handlePhoneInputChange = (event: ChangeEvent<HTMLInputElement>, index: number) => {
     const newPhoneNumber = event.target.value;
     const sanitizedPhoneNumber = newPhoneNumber.replace(/[^0-9]/g, ''); // Remove non-numeric characters
-    const newPhoneNumbers = [...contact.phoneNumbers];
+    const newPhoneNumbers = [...contact.phones];
     newPhoneNumbers[index] = sanitizedPhoneNumber;
 
     setContact((prevContact) => ({
       ...prevContact,
-      phoneNumbers: newPhoneNumbers,
+      phones: newPhoneNumbers,
     }));
   };
 
   const handleAddPhoneNumber = () => {
     setContact((prevContact) => ({
       ...prevContact,
-      phoneNumbers: [...prevContact.phoneNumbers, ''],
+      phones: [...prevContact.phones, ''],
     }));
   };
 
@@ -63,15 +79,16 @@ const Form: FC = () => {
     event.preventDefault();
 
     // Remove empty phone number entries
-    const sanitizedPhoneNumbers = contact.phoneNumbers.filter((phoneNumber) => phoneNumber.trim() !== '');
+    const sanitizedPhoneNumbers = contact.phones
+      .filter((phoneNumber) => phoneNumber.trim() !== '')
+      .map((phoneNumber) => ({ number: phoneNumber }));
 
-    const sanitizedContact: Contact = {
+    const sanitizedContact = {
       ...contact,
-      phoneNumbers: sanitizedPhoneNumbers,
+      phones: sanitizedPhoneNumbers,
     };
 
-    console.log('Contact:', sanitizedContact);
-    // You can perform further actions with the sanitized contact data, like sending it to an API.
+    addContact({ variables: sanitizedContact });
   };
 
   return (
@@ -79,24 +96,24 @@ const Form: FC = () => {
       <Header
         type={HeaderType.Form}
         onClickRightButton={handleSubmit}
-        disableRightButton={!isValid}
+        disableRightButton={!isValid || loading}
       />
       <div className={Styles.container}>
         <FormInput
-          name="firstName"
-          value={contact.firstName}
+          name="first_name"
+          value={contact.first_name}
           onChange={handleInputChange}
           placeholder="First name"
         />
         <div className={Styles.spacer(8)} />
         <FormInput
-          name="lastName"
-          value={contact.lastName}
+          name="last_name"
+          value={contact.last_name}
           onChange={handleInputChange}
           placeholder="Last name"
         />
         <div className={Styles.spacer(32)} />
-        {contact.phoneNumbers.map((phoneNumber, index) => (
+        {contact.phones.map((phoneNumber, index) => (
           <div key={index}>
             <FormInput
               value={phoneNumber}
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -39,3 +39,30 @@ export const GET_CONTACT_DETAILS = gql`
     }
   }
 `;
+
+export const ADD_CONTACT_WITH_PHONES = gql`
+  mutation AddContactWithPhones(
+    $first_name: String!,
+    $last_name: String!,
+    $phones: [phone_insert_input!]!
+  ) {
+    insert_contact(
+        objects: {
+            first_name: $first_name,
+            last_name:
+            $last_name, phones: {
+                data: $phones
+              }
+          }
+      ) {
+      returning {
+        first_name
+        last_name
+        id
+        phones {
+          number
+        }
+      }
+    }
+  }
+`;
